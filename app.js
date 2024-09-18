50ea5b05e8cc74f91f2022d2ec4254fc7f949ff9
Alejandro Barrera <alejandro91dark@hotmail.es>
2024-09-18 17:07:39 -0300

    commit 2

---
+++ app.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// cargar archivos rutas
+
+// middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// CORS
+
+// rutas
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Pagina de inicio</h1>");
+});
+
+app.post("/test/:id", (req, res) => {
+  console.log(req.body.nombre);
+  console.log(req.query.web);
+  console.log(req.params.id);
+
+  res.status(200).send({
+    message: "Hola mundo desde mi API de NodeJS",
+  });
+});
+
+// export
+module.exports = app;
