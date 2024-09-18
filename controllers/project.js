bfeed9f582f2d15a919de266cc7eda6fb74dd99c
Alejandro Barrera <alejandro91dark@hotmail.es>
2024-09-18 19:10:55 -0300

    commit 3

---
+++ controllers/project.js
@@ -0,0 +1,49 @@
+"use strict";
+
+var Project = require("../models/project");
+
+var controller = {
+  home: function (req, res) {
+    return res.status(200).send({
+      message: "Soy la home",
+    });
+  },
+
+  test: function (req, res) {
+    return res.status(200).send({
+      message: "Soy el metodo o accion test del controlador de project",
+    });
+  },
+
+  saveProject: function (req, res) {
+    var project = new Project();
+    var params = req.body;
+
+    project.name = params.name;
+    project.description = params.description;
+    project.category = params.category;
+    project.year = params.year;
+    project.langs = params.langs;
+    project.image = null;
+
+    res.status(200).send({});
+
+    project.save((err, projectStored) => {
+      console.log(err);
+
+      if (err)
+        return res
+          .status(500)
+          .send({ message: "Error al guardar el documento" });
+
+      if (!projectStored)
+        return res
+          .status(404)
+          .send({ message: "No se ha podido guardar el proyecto" });
+
+      return res.status(200).send({ project: projectStored });
+    });
+  },
+};
+
+module.exports = controller;
